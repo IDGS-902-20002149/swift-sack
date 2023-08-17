bb4098d0e865c0f807d037ec23b11479f8c47ab2
Luis German Vargas Hernandez <gercool92@gmail.com>
2023-08-17 11:41:22 -0600

    Se suben validaciones en los campos y diseño en los formularios

--- src/app/modulos/productosC/productosC/productosC.component.ts
+++ src/app/modulos/productosC/productosC/productosC.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ProductoSS } from 'src/app/interfaces/swiftsack';
 import { ProyectoApiService } from 'src/app/proyecto-api.service';
 import { FormsModule } from '@angular/forms';
+import { UsuarioMod } from 'src/app/interfaces/usuario';
+import { Router } from '@angular/router';
+import { Carrito } from 'src/app/interfaces/carrito';
 
 @Component({
   selector: 'app-productos',
@@ -15,6 +18,16 @@ export class ProductosCComponent {
   listFilter:string='';
   selectedPriceRange: string = '';
 
+  usuario:UsuarioMod = {
+    id: 0,
+    name: '0',
+    email: '0',
+    password: '0',
+    telefono: '0',
+    active: false,
+    confirmed_at: '0',
+    roleId: 0,
+  };
 
   onPriceRangeChange() {
     if (this.selectedPriceRange != 'all') {
@@ -23,7 +36,7 @@ export class ProductosCComponent {
   }
 
 
-  constructor(public objApi:ProyectoApiService){}
+  constructor(public objApi:ProyectoApiService, private router: Router){}
 
   getCurrentPageItems(): any[] {
     const startIndex = (this.currentPage - 1) * this.pageSize;
@@ -52,6 +65,41 @@ export class ProductosCComponent {
   }
 
   ngOnInit(): void {
+    this.obtenerUsuario();
+    if(this.usuario.roleId != 3){
+      this.router.navigate(['/home']);
+    }
     this.cargarProductos();
+  }
+
+  addCarrito(cantidad:number, productId:number){
+    const carrito:Carrito = {
+      idCarrito:0,
+      userId: this.usuario.id,
+      productId: productId,
+      cantidad: cantidad
+    }
+
+    console.log(carrito);
+
+    this.objApi.addCarrito(carrito).subscribe({
+      next: () => {
+        console.log('Producto agregado al carrito');
+        this.router.navigate(['PreviewCar']);
+      },
+      error: (e) => console.error(e),
+      complete: () => console.info('Solicitud completada')
+    });
+  }
+
+  obtenerUsuario(){
+    const userData = sessionStorage.getItem('userData');
+
+    if (userData) {
+      this.usuario = JSON.parse(userData);
+      console.log('Usuario: ' + this.usuario.name + ' recuperado');
+    } else {
+      console.log('El objeto no fue encontrado en sessionStorage.');
+    }
   }
 }
